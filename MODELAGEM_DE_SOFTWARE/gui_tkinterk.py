04f3656f6711c134b3e37508641e780cbcf0677e
Fábio Luiz <fabio__luiz@hotmail.com>
2026-07-02 21:20:35 -0300

    Janela principal

--- MODELAGEM_DE_SOFTWARE/gui_tkinterk.py
+++ MODELAGEM_DE_SOFTWARE/gui_tkinterk.py
@@ -1,1 +1,15 @@
+import tkinter as tk
+from tkinter import Tk
+
+janela = tk.Tk()
+janela.title("Financiamento de imóveis")
+janela.geometry("400x200")
+
+label = tk.Label(janela, text = "Insira sua matricula", font=("Arial",20))
+label.pack(pady=20, padx=20)
+texto = tk.Entry(janela,width=20)
+texto.pack(pady=20)
+
+janela.mainloop()
+
 
